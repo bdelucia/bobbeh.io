cfdbbb51030f10969668d3474e495199e98eb03d
Bobby DeLucia (endeavorOS) <bdelucia@asu.edu>
2026-07-30 18:20:05 -0700

    keystatic github mode

--- keystatic.config.ts
+++ keystatic.config.ts
@@ -2,7 +2,11 @@ import { config, fields, collection, singleton } from "@keystatic/core";
 
 export default config({
   storage: {
-    kind: "local",
+    kind: "github",
+    repo: {
+      owner: "bdelucia",
+      name: "bobbeh.io",
+    },
   },
 
   collections: {
